0bc0eb5c4e92d1af16821ebbc89e65cf2bee726d
rekusha <rekusha@gmail.com>
2020-10-07 12:27:54 +0300

    8 - ajax site parsing. finish

--- 08-parsing_site_with_ajax/main.py
+++ 08-parsing_site_with_ajax/main.py
@@ -1,15 +1,41 @@
+# XHR запросы отдают чистый текст без хтмл кода, поэтому bs4 не нужен просто обрабатываем ответ сервера как строки
+
 import requests
-from bs4 import BeautifulSoup
 import csv
-from peewee import *
 
 
 def get_html(url):
-    r = requests.get(url)
+    return requests.get(url).text
+
+
+def write_csv(data):
+    with open('sites.csv', 'a', newline='', encoding='utf-8') as f:
+        order = ['name', 'url', 'description', 'traffic', 'percent']
+        writer = csv.DictWriter(f, fieldnames=order)
+        writer.writerow(data)
 
 
 def main():
-    url = ''
+    for i in range(1, 6286):
+        url = 'https://www.liveinternet.ru/rating/ru//today.tsv?page={}'  # урл по запросу на который вернется текст
+        response = get_html(url.format(i))  # в url одставляется i вместо фигурных скобок (.формат)
+        data = response.strip().split('\n')[1:]  # чистим строку с концов от спец символов и мусора, делим по переходам
+        # строк '\n' и отбрасываем первый элемент списка [1:]
+        for row in data:  # в цикле прокручиваем весь список
+            column = row.strip().split('\t')  # делим каждую строку на составные части по признаку символа табуляции
+            name = column[0]
+            url = column[1]
+            description = column[2]
+            traffic = column[3]
+            percent = column[4]
+
+            data = {'name': name,  # создаем словарь с нужными полями и данными для записи в csv
+                    'url': url,
+                    'description': description,
+                    'traffic': traffic,
+                    'percent': percent}
+
+            write_csv(data)  # отправляем словарь в csv для записи
 
 
 if __name__ == '__main__':
