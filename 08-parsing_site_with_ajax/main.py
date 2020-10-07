5ba4c1656b4e3eb39dfa5e98f2417828a574004a
rekusha <rekusha@gmail.com>
2020-10-07 11:02:05 +0300

    8 - ajax site parsing. start

---
+++ 08-parsing_site_with_ajax/main.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+from peewee import *
+
+
+def get_html(url):
+    r = requests.get(url)
+
+
+def main():
+    url = ''
+
+
+if __name__ == '__main__':
+    main()
