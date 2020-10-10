797b2eae33298ba33381b3c7d066a9d8e7aac3c8
rekusha <rekusha@gmail.com>
2020-10-10 23:52:09 +0300

    14 parsing with authorizations by requests. start

---
+++ 14-requests_eith_authorization/main.py
@@ -0,0 +1,13 @@
+import requests
+
+from config import username, password  # from config.py import username and password in string format
+
+
+
+
+def main():
+    url=''
+
+
+if __name__ == '__main__':
+    main()
